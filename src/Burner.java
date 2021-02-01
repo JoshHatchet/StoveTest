eba9635c985fdaf91bf0456436ca85481b9a69ad
JoshHatchet <joshbordick@gmail.com>
2021-02-01 10:44:11 -0700

    updated minus and plus buttons in Burner.java

--- src/Burner.java
+++ src/Burner.java
@@ -1,7 +1,54 @@
 
 public class Burner {
-	private String stuff = "Void Stuff";
-	stuff = "change";
-	//draw
+	
+	public final static int TIME_DURATION = 2;
+	Setting mySetting = Setting.OFF;
+	Temperature myTemperature = Temperature.COLD;
+	
+	
+	public enum Temperature{
+		BLAZING, HOT, WARM, COLD;
+		
+	}
+	
+	public void minusButton() {
+		Setting current = mySetting;
+		switch(current) {
+		case OFF:
+			break;
+		case LOW:
+			mySetting = Setting.OFF;
+			break;
+		case MEDIUM:
+			mySetting = Setting.LOW;
+			break;
+		case HIGH:
+			mySetting = Setting.MEDIUM;
+			break;
+		}
+	}
+	
+	public void plusButton() {
+		Setting curr = mySetting;
+		switch(curr) {
+		case OFF:
+			mySetting = Setting.LOW;
+			break;
+		case LOW:
+			mySetting = Setting.MEDIUM;
+			break;
+		case MEDIUM:
+			mySetting = Setting.HIGH;
+			break;
+		case HIGH:
+			break;
+		}
+	}
+	
+	
+	
+	
+	
+	
 	
 }
