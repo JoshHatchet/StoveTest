909e71d4c6eef03efa3a9c3343661596657ab16f
JoshHatchet <joshbordick@gmail.com>
2021-02-01 10:11:00 -0700

    Completed enum type Setting and methods

--- src/Setting.java
+++ src/Setting.java
@@ -1,4 +1,14 @@
 
 public enum Setting {
+	OFF("---"), LOW("--+"), MEDIUM("-++"), HIGH("+++");
+	
+	private String string;
 
+	Setting(String string) {
+		this.string = string;
+	}
+	public String toString() {
+		return string; 
+	}
 }
+	
